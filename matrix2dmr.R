d1aec3c72fb1ec8bbb0cad3ea86e4ba166ddf256
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-24 17:35:53 -0600

    Create matrix2dmr.R

---
+++ matrix2dmr.R
@@ -0,0 +1,27 @@
+dir=getwd()
+ttestFunction<-function(data,x1,x2){
+  output<-matrix(NA,dim(data)[1],4)
+  data=data+matrix(abs(rnorm(nrow(data)*ncol(data),0,0.0001)),nrow(data),ncol(data))
+  for(i in 1:dim(data)[1]){
+    out<-data.frame()
+    if(all(! any(all(is.na(data[i,x1])),sum(! is.na(data[i,x1]))<2,sum(! is.na(data[i,x2]))<2,all(is.na(data[i,x2]))),sum(is.na(data[i,]))<0.5*length(data[i,]))){
+      tmp1<-try(t.test(as.numeric(data[i,x1]),as.numeric(data[i,x2]), na.action=na.omit))
+      output[i,1]<-tmp1$p.value
+      output[i,2]<-as.numeric((mean(as.numeric(data[i,x1]),na.rm=T)-mean(as.numeric(data[i,x2]),na.rm=T)))
+      output[i,3]<-mean(as.numeric(data[i,x1]),na.rm=T)
+      output[i,4]<-mean(as.numeric(data[i,x2]),na.rm=T)
+      print(i)
+    }
+  }
+  rownames(output)<-rownames(data)
+  P.Adj<-p.adjust(output[,1],method="fdr")
+  out<-data.frame(output[,1],P.Adj,output[,2:4])
+  colnames(out)=c("Pvalue","PFDR","Delta","RA","OA")
+  return(out)
+}
+
+args = commandArgs(trailingOnly=TRUE)
+temp<-read.table(args[1],head=T,row.names = 1)
+rlt<-ttestFunction(temp,1:9,10:19)
+write.table(rlt,file=paste(args[1],".pvalue.txt",sep=""),col.names = NA,row.names = T,sep="\t",quote=F)
+save(rlt,file=paste(file[i],".pvalue.RData",sep=""))
